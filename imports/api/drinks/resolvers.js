99ce8296758ddcfc83eda833aefe1666d3689515
Will Luft <william.d.luft@gmail.com>
2020-06-11 11:16:00 -0400

    Updated createDrink resolver to include tags

--- imports/api/drinks/resolvers.js
+++ imports/api/drinks/resolvers.js
@@ -1,4 +1,5 @@
 import Drinks from './drinks';
+import Tags from '../tags/tags';
 
 export default {
   Query: {
@@ -11,10 +12,34 @@ export default {
   },
 
   Mutation: {
-    createDrink(obj, { name }, context) {
-      console.log(name);
+    createDrink(obj, { drink }, context) {
+      console.log(drink.name);
+      console.log(drink.tags[0].name);
+      // Get list of current tags
+      const currentTagsList = Tags.find({}).fetch();
+
+      let drinkTags = [];
+      // for each tag in new drink
+      drink.tags.forEach(tag => {
+        console.log(`current tag: ${tag.name}`);
+        // check if tag already exists
+        const exists = currentTagsList.find(
+          currentTag => currentTag.name === tag.name
+        );
+        // if exists, add tag id to array
+        if (exists) {
+          drinkTags.push(exists);
+        }
+        // if tag doesn't exist, create new tag
+        const tagId = Tags.insert({
+          name: tag.name,
+        });
+        drinkTags.push(Tags.findOne(tagId));
+      });
+
       const drinkId = Drinks.insert({
-        name,
+        name: drink.name,
+        tags: drinkTags,
       });
       console.log(Drinks.findOne(drinkId));
       return Drinks.findOne(drinkId);
