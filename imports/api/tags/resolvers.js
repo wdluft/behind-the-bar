d29739b6bcae362bf7f3c018cc021dbfdbecc7d7
Will Luft <william.d.luft@gmail.com>
2020-06-20 14:06:28 -0400

    Added query and resolvers to get drinks by tag

--- imports/api/tags/resolvers.js
+++ imports/api/tags/resolvers.js
@@ -1,9 +1,26 @@
 import Tags from './tags';
+import Drinks from '../drinks/drinks';
 
 export default {
   Query: {
     tags() {
       return Tags.find({}).fetch();
+    },
+    drinksByTag(obj, { id }, context, info) {
+      // id is the tag id
+      // get array of drinks
+      const drinksList = Drinks.find({}).fetch();
+
+      // for each drink, if array of tags includes id, add to array of drinks to return
+      const drinksWithTag = [];
+      drinksList.forEach(drink => {
+        drink.tags.forEach(tag => {
+          if (tag._id === id) {
+            drinksWithTag.push(drink);
+          }
+        });
+      });
+      return drinksWithTag;
     },
   },
 
--- imports/api/tags/tags.js
+++ imports/api/tags/tags.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-unresolved
 import { Mongo } from 'meteor/mongo';
 
 const Tags = new Mongo.Collection('tags');
